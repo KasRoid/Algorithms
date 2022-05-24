d8c332041e40acc574be2e3b14e17b206f940c0b
KasRoid <KasRoid@gmail.com>
2022-05-25 06:50:05 +0900

    new: Solved Number at K Problem

---
+++ Programmers/Level_1/53.number_at_k.py
@@ -0,0 +1,9 @@
+# K번째수
+# https://programmers.co.kr/learn/courses/30/lessons/42748
+
+
+def solution(array, commands):
+    return [sorted(array[c[0] - 1: c[1]])[c[2] - 1] for c in commands]
+
+
+print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))  # [5, 6, 3]
