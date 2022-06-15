bb657ea4e5244d674fe94aefe6ced922985a7aec
KasRoid <KasRoid@gmail.com>
2022-06-15 16:23:20 +0900

    new: Solved N-Queen Problem

--- Programmers/Level_2/13.n_queen.py
+++ Programmers/Level_2/13.n_queen.py
@@ -2,8 +2,26 @@
 # https://programmers.co.kr/learn/courses/30/lessons/12952
 
 def solution(n):
-    answer = 0
-    return answer
+    count = [0]
+    for loc in range(n):
+        queens = []
+        dfs(queens, loc, count, n)
+    return count[0]
+
+
+def dfs(queens, loc, count, n):
+    if loc in queens:
+        return
+    for r, c in enumerate(queens):
+        h = len(queens) - r
+        if loc == c - h or loc == c + h:
+            return
+    queens.append(loc)
+    if len(queens) == n:
+        count[0] += 1
+        return
+    for loc in range(n):
+        dfs(queens.copy(), loc, count, n)
 
 
 print(solution(4))  # 2
