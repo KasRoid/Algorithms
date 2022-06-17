d060d3c36ab67cca8bed1df71eb59679c7dfb38f
KasRoid <KasRoid@gmail.com>
2022-06-18 08:59:16 +0900

    feat: Solved Tower of Hanoi Problem

--- Programmers/Level_2/14.tower_of_hanoi.py
+++ Programmers/Level_2/14.tower_of_hanoi.py
@@ -2,8 +2,18 @@
 # https://programmers.co.kr/learn/courses/30/lessons/12946
 
 def solution(n):
-    answer = [[]]
+    answer = []
+    hanoi(n, 1, 3, answer)
     return answer
+
+
+def hanoi(n, start, end, answer):
+    if n == 0:
+        return
+    vacant = 6 - start - end
+    hanoi(n - 1, start, vacant, answer)
+    answer.append([start, end])
+    hanoi(n - 1, vacant, end, answer)
 
 
 print(solution(2))  # [ [1,2], [1,3], [2,3] ]
