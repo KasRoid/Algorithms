f9ec547bf09d59ddcdff7e5c4ab71d78dbdd62e0
KasRoid <KasRoid@gmail.com>
2022-06-20 17:39:35 +0900

    new: Start Solving Number Block Problem

---
+++ Programmers/Level_2/16.number_block.py
@@ -0,0 +1,9 @@
+# 숫자 블록
+# https://programmers.co.kr/learn/courses/30/lessons/12923
+
+def solution(begin, end):
+    answer = []
+    return answer
+
+
+print(solution(1, 10))  # [0, 1, 1, 2, 1, 3, 1, 4, 3, 5]
