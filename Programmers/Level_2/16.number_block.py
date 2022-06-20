1ecaa505e0cd095a93940e92cf80cfe47b80c43d
KasRoid <KasRoid@gmail.com>
2022-06-21 08:39:59 +0900

    feat: Solved Number Block Problem

--- Programmers/Level_2/16.number_block.py
+++ Programmers/Level_2/16.number_block.py
@@ -2,8 +2,18 @@
 # https://programmers.co.kr/learn/courses/30/lessons/12923
 
 def solution(begin, end):
-    answer = []
-    return answer
+    return [0 if num == 1 else divisor(num) for num in range(begin, end + 1)]
+
+
+def divisor(num):
+    result = 1
+    for divider in range(2, int(num ** 0.5) + 1):
+        if num // divider > 10 ** 7:
+            continue
+        if num % divider == 0:
+            result = int(num / divider)
+            break
+    return result
 
 
 print(solution(1, 10))  # [0, 1, 1, 2, 1, 3, 1, 4, 3, 5]
