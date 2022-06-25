6d5c1f3492ec4e804b8e3dfed35105f46e271f77
KasRoid <KasRoid@gmail.com>
2022-06-25 09:49:29 +0900

    new: Start Solving Sort by Filename Problem

---
+++ Programmers/Level_2/20.sort_by_filename.py
@@ -0,0 +1,13 @@
+# 파일명 정렬
+# https://programmers.co.kr/learn/courses/30/lessons/17686
+
+
+def solution(files):
+    answer = []
+    return answer
+
+
+print(solution(["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]))  
+# ["img1.png", "IMG01.GIF", "img02.png", "img2.JPG", "img10.png", "img12.png"]
+print(solution(["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"]))
+# ["A-10 Thunderbolt II", "B-50 Superfortress", "F-5 Freedom Fighter", "F-14 Tomcat"]
